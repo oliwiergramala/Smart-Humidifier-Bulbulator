f22423bdb13f24f1475377ad3f95fcbc9621b5e8
oliwier <olivier.gramala@gmail.com>
2026-07-29 18:24:25 +0200

    feat: add idf.py package

--- devenv.nix
+++ devenv.nix
@@ -2,18 +2,22 @@
   pkgs,
   lib,
   config,
+  inputs,
   ...
 }:
 {
   # https://devenv.sh/packages/
   packages = [
-    pkgs.esptool
-    pkgs.cargo-espmonitor
+    pkgs.cmake
+    pkgs.ninja
     pkgs.go-task
+    inputs.esp-dev.packages.${pkgs.system}.esp-idf-full
   ];
 
   # https://devenv.sh/languages/
   languages.c.enable = true;
+
+  languages.python.enable = true; 
 
   # See full reference at https://devenv.sh/reference/options/
 }
